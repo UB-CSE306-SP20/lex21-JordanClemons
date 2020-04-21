b24f63f41078854cbc6b0e5814184d54c655b428
Jordan A Clemons <jaclemon@timberlake.cse.buffalo.edu>
2020-04-21 15:32:27 -0400

    Stub out all functions

---
+++ blocksWorld.c
@@ -0,0 +1,31 @@
+#include "blocksWorld.c"
+
+int * create(int n){
+  return NULL;
+}
+
+
+bool onTable(int m){
+  return false;
+}
+
+
+bool open(int m){
+  return false;
+}
+
+
+int on(int m){
+  return -1;
+}
+
+
+bool above(int m, int n){
+  return false;
+}
+
+
+void move(int m, int n){
+  return;
+}
+
---
+++ blocksWorld.h
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+
+
+int * create(int n); //Returns array of size n and sets each block to the table 
+
+
+
+bool onTable(int m);  //Returns true if block m is on the table, false otherwise
+
+
+
+bool open(int m);  //Returns true if there is no block on m, false otherwise
+
+
+
+int on(int m);  //returns 0 if m is on the table and n if m is on block n, if error return m
+
+
+
+bool above(int m, int n);  //returns true if m is directly on n, or if m is directly on x and x is above n, false otherwise
+
+
+void move(int m, int n);  //moves m onto n iff both blocks are open and in range, no effect otherwise
+
+  
